cedb228c2c36bfa203b4ab3e59e3baa4ba24d3ee
Eric Ung <eu2138@nyu.edu>
2025-09-15 19:19:33 -0500

    09152025-print works

--- webServer.py
+++ webServer.py
@@ -36,21 +36,19 @@ def webServer(port=13331):
 
             message += f.read()
 
-
-
             #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
             #Fill in start 
                   
             #Content-Type is an example on how to send a header as bytes. There are more!
-        #    outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+            outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
 
 
             #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
 
             #Fill in end
 
-        #    for i in f: #for line in file
-        #        print("hello")
+            for i in f: #for line in file
+                print("hello")
             #    print(f)
             #    print(i)
             #Fill in start - append your html file contents #Fill in end 
